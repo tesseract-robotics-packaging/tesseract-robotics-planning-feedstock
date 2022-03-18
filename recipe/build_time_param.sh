c65049e49e688021b6b7f7191f06d132a9f2eeba
John Wason <wason@wasontech.com>
2022-03-17 20:23:59 -0400

    Add patch, fix typo

--- recipe/build_time_param.sh
+++ recipe/build_time_param.sh
@@ -7,7 +7,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DBUILD_SHARED_LIBS=ON \
-  -S src/tesseract_tesseract_time_parameterization \
+  -S src/tesseract_time_parameterization \
   -B build_time_param_dir
 
 cmake --build build_time_param_dir --config Release -- -j$CPU_COUNT
